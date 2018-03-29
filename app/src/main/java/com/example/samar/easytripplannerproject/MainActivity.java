fe1fa6d57a0fded8b211e1d828ee20a240249da0
samar <samarnasef80@gmail.com>
2018-03-29 16:38:55 +0200

    create login activity

--- app/src/main/java/com/example/samar/easytripplannerproject/MainActivity.java
+++ app/src/main/java/com/example/samar/easytripplannerproject/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.samar.easytripplannerproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button logIn;
+    Button signUp;
+    Intent loginIntent;
+    Intent signupIntent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logIn = findViewById(R.id.Login);
+        signUp =findViewById(R.id.signup);
+
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                loginIntent = new Intent(MainActivity.this,loginActivity.class);
+                startActivity(loginIntent);
+
+
+
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
     }
 }
